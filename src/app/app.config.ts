884748fef35b5ec2ce59f5292597314b2a0993a6
Ilya <riper752@gmail.com>
2024-10-22 23:53:35 +0300

    Added angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
 import { provideStore } from '@ngxs/store';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore([], withNgxsReduxDevtoolsPlugin())],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore([], withNgxsReduxDevtoolsPlugin()),
+    provideAnimationsAsync(),
+  ],
 };
