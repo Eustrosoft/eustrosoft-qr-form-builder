1e3a70b58ad3bd15a3005aa0ea5ea30470123c54
Ilya <riper752@gmail.com>
2024-11-05 18:36:04 +0300

    Added overflow for editor

--- src/app/pages/form-constructor/form-constructor.constant.ts
+++ src/app/pages/form-constructor/form-constructor.constant.ts
@@ -2,26 +2,26 @@ import { DatepickerElement, InputElement, SelectElement } from '@app/pages/form-
 import { ConnectedPosition } from '@angular/cdk/overlay';
 
 export const INPUT_DEFAULT_PARAMS: InputElement = {
+  controlElement: 'input',
   label: 'Input',
   placeholder: '',
   hint: '',
-  controlElement: 'input',
   inputType: 'text',
 } as const;
 
 export const SELECT_DEFAULT_PARAMS: SelectElement = {
+  controlElement: 'select',
   label: 'Select',
   placeholder: '',
   hint: '',
-  controlElement: 'select',
   optionList: [],
 } as const;
 
 export const DATEPICKER_DEFAULT_PARAMS: DatepickerElement = {
+  controlElement: 'datepicker',
   label: 'Datepicker',
   placeholder: '',
   hint: '',
-  controlElement: 'datepicker',
   datepickerType: 'single',
 } as const;
 
